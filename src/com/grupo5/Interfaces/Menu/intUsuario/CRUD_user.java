84217721f4224ebd6ed9fdf8c3c637bd0e557529
ludwin <ludwindanieltello@hotmail.com>
2021-07-01 00:33:05 -0600

    Actualizacion Gui Estructura general parte 1, eventos en botones

--- src/com/grupo5/Interfaces/Menu/intUsuario/CRUD_user.java
+++ src/com/grupo5/Interfaces/Menu/intUsuario/CRUD_user.java
@@ -10,6 +10,8 @@ import com.sun.istack.internal.NotNull;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 
@@ -18,6 +20,7 @@ public class CRUD_user extends JPanel {
     public Color fondo = new Color(24, 30, 54);
     public Color azul = new Color(42, 52, 67);
     public Color texto = new Color(0, 126, 249);
+    public JTable table;
     public Color textoSecundario = new Color(158, 161, 176);
 
     public CRUD_user(Gestor_usuario user) {
@@ -35,24 +38,31 @@ public class CRUD_user extends JPanel {
 
         ArrayList<Usuario> data = usuario.getArray();
 
-        Object [] header = new Object[]{"Usuario", "Password", "", ""};
+        Object[] header = new Object[]{"Usuario", "Password", "", ""};
 
 
-
-        Object matriz[][]  = new Object[data.size()][4];
-        JLabel modificar = new JLabel("Modificar");
+        Object matriz[][] = new Object[data.size()][4];
+        JButton modificar = new JButton("Modificar");
+        modificar.setName("m");
         modificar.setForeground(textoSecundario);
-        modificar.setBounds(0,0,30,30);
-        modificar.setIcon(getIcon2("iconos\\update.png",modificar));
+        modificar.setBorder(null);
+        modificar.setBackground(azul);
+        modificar.setBounds(0, 0, 30, 30);
+        modificar.setIcon(getIcon2("iconos\\update.png", modificar));
         modificar.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        modificar.setFont(fuente.fuente(fuente.OpensansBold, 0, 13));
 
-        JLabel eliminar = new JLabel("Eliminar");
-
-        eliminar.setBounds(0,0,30,30);
+        JButton eliminar = new JButton("Eliminar");
+        eliminar.setName("e");
         eliminar.setForeground(textoSecundario);
-        eliminar.setIcon(getIcon2("iconos\\delete2.png",modificar));
+        eliminar.setBorder(null);
+        eliminar.setBackground(azul);
+        eliminar.setBounds(0, 0, 30, 30);
+        eliminar.setForeground(textoSecundario);
+        eliminar.setIcon(getIcon2("iconos\\delete2.png", modificar));
         eliminar.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        for (int i = 0; i<data.size(); i++){
+        eliminar.setFont(fuente.fuente(fuente.OpensansBold, 0, 13));
+        for (int i = 0; i < data.size(); i++) {
 
             matriz[i][0] = data.get(i).getUsername();
             matriz[i][1] = data.get(i).getPassword();
@@ -61,40 +71,61 @@ public class CRUD_user extends JPanel {
 
         }
 
-        DefaultTableModel model = new DefaultTableModel(matriz, header){
-            public boolean isCellEditable(int row, int column){
+        DefaultTableModel model = new DefaultTableModel(matriz, header) {
+            public boolean isCellEditable(int row, int column) {
                 return false;
             }
         };
 
-        JTable table = new JTable(model);
+        table = new JTable(model);
         table.setForeground(textoSecundario);
         table.getTableHeader().setForeground(textoSecundario);
         table.getTableHeader().setDefaultRenderer(new HeaderRenderer(table));
-        table.setDefaultRenderer(Object.class,new Render());
+        table.setDefaultRenderer(Object.class, new Render());
         table.setFocusable(false);
         table.setBackground(azul);
-        table.setIntercellSpacing(new Dimension(0,1));
+        table.setIntercellSpacing(new Dimension(1, 1));
         table.setRowHeight(50);
-       table.setSelectionBackground(texto);
+        table.setSelectionBackground(texto);
         table.setShowVerticalLines(false);
         table.setBorder(BorderFactory.createEmptyBorder());
+        table.setFont(fuente.fuente(fuente.OpensansBold, 0, 13));
 
-
-        table.getTableHeader().setFont(fuente.fuente(fuente.OpensansBold, 0,17));
+        table.getTableHeader().setFont(fuente.fuente(fuente.OpensansBold, 0, 17));
         table.getTableHeader().setOpaque(true);
 
-       table.getTableHeader().setBorder(BorderFactory.createEmptyBorder());
-       table.getTableHeader().setBackground(azul);
-       table.getTableHeader().setResizingAllowed(false);
-       table.setGridColor(azul);
+        table.getTableHeader().setBorder(BorderFactory.createEmptyBorder());
+        table.getTableHeader().setBackground(azul);
+        table.getTableHeader().setResizingAllowed(false);
+        table.setShowHorizontalLines(true);
+        table.setGridColor(texto);
 
-        UIManager.getDefaults().put("TableHeader.cellBorder" , BorderFactory.createEmptyBorder(0,0,0,0));
+
+        UIManager.getDefaults().put("TableHeader.cellBorder", BorderFactory.createEmptyBorder(0, 0, 0, 0));
 
         //table.getColumnModel().getColumn(2).setCellRenderer(new ButtonRenderer());
         table.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        table.setBounds(80,100,400,400);
+        table.setBounds(80, 100, 400, 400);
+        table.addMouseListener(new MouseAdapter() {
 
+            public void mouseClicked(MouseEvent e) {
+                int col = table.getColumnModel().getColumnIndexAtX(e.getX());
+                int row = e.getY() / table.getRowHeight();
+                if (row < table.getRowCount() && row >= 0 && col < table.getColumnCount() && col > 0) {
+                    Object valor = table.getValueAt(row, col);
+                    if (valor instanceof JButton) {
+                        ((JButton)valor).doClick();
+                        JButton btn = (JButton) valor;
+                        if (btn.getName().equals("m")) {
+                            System.out.println("Modificar");
+                        }
+                        if (btn.getName().equals("e")) {
+                            System.out.println("Eliminar");
+                        }
+                    }
+                }
+            }
+        });
         JScrollPane pane = new JScrollPane();
         pane.setViewportView(table);
         pane.setBackground(azul);
@@ -102,18 +133,20 @@ public class CRUD_user extends JPanel {
         pane.setOpaque(true);
         pane.setBorder(BorderFactory.createEmptyBorder());
         pane.getViewport().setBackground(azul);
-        pane.setBounds(150,100,600,400);
+        pane.setBounds(150, 100, 600, 400);
 
         add(pane);
 
 
     }
-    ImageIcon getIcon2(String ruta, @NotNull JLabel label) {
+
+    ImageIcon getIcon2(String ruta, @NotNull JButton label) {
         ImageIcon icon = new ImageIcon(ruta);
         Image img = icon.getImage();
         Image imgs = img.getScaledInstance(label.getWidth(), label.getHeight(), Image.SCALE_SMOOTH);
         ImageIcon scale = new ImageIcon(imgs);
         return scale;
     }
+
 
 }
