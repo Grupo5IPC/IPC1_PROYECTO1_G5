e9ea4e8732080b0d8ea350c1c88948b7d5917f3c
L-Tello <81892001+L-Tello@users.noreply.github.com>
2021-07-05 16:04:24 -0600

    Create mascotas.js

---
+++ mascotas.js
@@ -0,0 +1,52 @@
+[
+  {
+    "id": 1,
+    "nombre": "Tobi",
+    "edad": 10,
+    "dueños": [
+      {
+        "nombre": "Ludwin",
+        "apellido": "Tello",
+        "nombresFavoritos": [
+          "Tobias",
+          "Gordo",
+          "Chucho"
+        ]
+      },
+      {
+        "nombre": "Daniel",
+        "apellido": "Rodriguez",
+        "nombresFavoritos": [
+          "Tobito",
+          "Flaco",
+          "Perro"
+        ]
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "nombre": "Irkan",
+    "edad": 2,
+    "dueños": [
+      {
+        "nombre": "Marvin",
+        "apellido": "Tello",
+        "nombresFavoritos": [
+          "Gordo",
+          "Irkan",
+          "Cito"
+        ]
+      },
+      {
+        "nombre": "Gabriel",
+        "apellido": "Rodriguez",
+        "nombresFavoritos": [
+          "Chucha",
+          "Irkaan",
+          "Irkaaan"
+        ]
+      }
+    ]
+  }
+]
